8f19fff1c575a8e5a6b80c76935a8e9a69313649
Leigh Halliday <leighhalliday@gmail.com>
2020-04-27 19:30:52 -0400

    Finishing up react virtualized demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  List,
+  AutoSizer,
+  CellMeasurer,
+  CellMeasurerCache,
+} from "react-virtualized";
+import faker from "faker";
 
-function App() {
+export default function App() {
+  const cache = React.useRef(
+    new CellMeasurerCache({
+      fixedWidth: true,
+      defaultHeight: 100,
+    })
+  );
+  const [people, setPeople] = React.useState([]);
+  const [time, setTime] = React.useState(new Date());
+
+  React.useEffect(() => {
+    setPeople(
+      [...Array(1000).keys()].map((key) => {
+        return {
+          id: key,
+          name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          bio: faker.lorem.lines(Math.random() * 100),
+        };
+      })
+    );
+  }, []);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{time.toISOString()}</h1>
+
+      <div style={{ width: "100%", height: "100vh" }}>
+        <AutoSizer>
+          {({ width, height }) => (
+            <List
+              width={width}
+              height={height}
+              rowHeight={cache.current.rowHeight}
+              deferredMeasurementCache={cache.current}
+              rowCount={people.length}
+              rowRenderer={({ key, index, style, parent }) => {
+                const person = people[index];
+
+                return (
+                  <CellMeasurer
+                    key={key}
+                    cache={cache.current}
+                    parent={parent}
+                    columnIndex={0}
+                    rowIndex={index}
+                  >
+                    <div style={style}>
+                      <h2>{person.name}</h2>
+                      <p>{person.bio}</p>
+                    </div>
+                  </CellMeasurer>
+                );
+              }}
+            />
+          )}
+        </AutoSizer>
+      </div>
+
+      {/* <ul>
+        {people.map((person) => (
+          <li key={person.id}>
+            <h2>{person.name}</h2>
+          </li>
+        ))}
+      </ul> */}
     </div>
   );
 }
-
-export default App;
